fb309336de83021637b74849d48a7a4ab558a458
Ashik Muhammed <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2021-12-09 15:42:30 +0530

    🦊

---
+++ pbot.py
@@ -0,0 +1,27 @@
+import os
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+
+Developedbots = Client(
+    "Pyrogram-example-bot",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
+
+
+START_BUTTON = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('SOURCE CODE', url="https://github.com/Developed-Bots/Pyrogram-example-bot")
+        ]]
+    ) 
+@Developedbots.on_message(filters.private & filters.command(["start"]))
+async def start(bot, update):
+    await message.reply_photo("https://telegra.ph/file/47d44ef6072f170652ef0.jpg")
+    await message.reply_text(
+        f""" Hai {update.from_user.mention} am just a pyrogram example bot""", 
+        disable_web_page_preview=True,
+        reply_markup=START_BUTTON
+    )
+
+Developedbots.run()
